9d5ac968e28b3ea40513ec46750ccb614a224dec
md-alif007 <mohammadalif043@gmail.com>
2025-07-04 19:42:08 +0600

    26 - 1 and 2 and 3

---
+++ eventListener/scripts/java.js
@@ -0,0 +1,11 @@
+function makeYellow(){
+    document.body.style.background = 'yellow';
+}
+
+// option 3
+
+const btnMakeRed = document.getElementById('btn-make-red');
+// console.log(btnMakeRed);
+btnMakeRed.onclick = function makeRed(){
+    document.body.style.background = 'Red';
+}
